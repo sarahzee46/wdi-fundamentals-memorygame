3a5a403c360c2f73e5f28c4b2010080add2ebe39
fazilmehdi <fazilmehd@gmail.com>
2020-03-05 16:23:05 +1100

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+let cardOne = "queen";
+let CardTwo = "queen";
+let cardThree = "king";
+let cardFour = "king";
+console.log("user flipped " + cardOne);
+console.log("user flipped " + cardThree);
