a9ee7e254a256515d70d77e713b6df1734598ff4
fazilmehdi <fazilmehd@gmail.com>
2020-03-10 12:05:09 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,26 @@
 let cards = ["queen","queen","king","king"];
 let cardsInPlay = [];
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("user flipped " + cardsInPlay);
-let cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("user flipped " + cardTwo);
-if(cardsInPlay.length===2)
+
+function checkForMatch() {
+	if(cardsInPlay.length===2)
 {
+
 	if(cardsInPlay[0]===cardsInPlay[1])
 		alert("you found a match!");
 	else
 		alert("sorry try again!");
 }
+
+}
+
+function flipCard(cardId) {
+
+console.log("user flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+checkForMatch();
+
+}
+flipCard(0);
+flipCard(2);
 
 
