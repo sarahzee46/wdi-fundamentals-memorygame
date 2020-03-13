75021bea23bf7e9191498f96bc875e9a4551d7ff
fazilmehdi <fazilmehd@gmail.com>
2020-03-13 11:44:46 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,8 +23,9 @@ const cards = [
 let cardsInPlay = [];
 
 function checkForMatch() {
-	if(cardsInPlay.length===2)
-{
+    
+	
+
 
 	if(cardsInPlay[0]===cardsInPlay[1])
 		alert("you found a match!");
@@ -32,18 +33,36 @@ function checkForMatch() {
 		alert("sorry try again!");
 }
 
-}
 
-function flipCard(cardId) {
 
+function flipCard() {
+
+let cardId = this.getAttribute('data-id');
 console.log("user flipped " + cards[cardId].rank);
 cardsInPlay.push(cards[cardId].rank);
-checkForMatch();
 console.log(cards[cardId].suit);
 console.log(cards[cardId].cardImage);
 
+this.setAttribute('src',cards[cardId].cardImage);
+if(cardsInPlay.length===2)
+	checkForMatch();
+
 }
-flipCard(0);
-flipCard(2);
+
+
+var createBoard = function() {
+	for(let i=0;i<cards.length;i++)
+	{
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src',"images/back.png");
+		cardElement.setAttribute('data-id',i);
+		cardElement.addEventListener('click',flipCard);
+
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+createBoard();
+	
+
 
 
